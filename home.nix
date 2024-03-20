90574301b4461f127c2024b64f2acc526eb29ef4
xanderm <xanderbruce1@gmail.com>
2024-03-20 12:59:10 -0700

    added vesktop

--- home.nix
+++ home.nix
@@ -40,6 +40,7 @@
     pkgs.swww
     pkgs.firefox
     pkgs.alacritty-theme
+    pkgs.vesktop
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
