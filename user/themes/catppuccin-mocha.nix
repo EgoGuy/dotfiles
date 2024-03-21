70eb8d8a845a03d75d616e2267bf60aa56dee8d1
xanderm <xanderbruce1@gmail.com>
2024-03-20 22:23:19 -0700

    set up cursors

--- user/themes/catppuccin-mocha.nix
+++ user/themes/catppuccin-mocha.nix
@@ -12,6 +12,18 @@
 	variant = "mocha";
       };
     };
+    cursorTheme = {
+      name = "Catppuccin-Mocha-Mauve-Cursors";
+      package = pkgs.catppuccin-cursors.mochaMauve;
+      size = 24;
+    };
+  };
+
+  # cursor config
+  home.pointerCursor = {
+    package = pkgs.catppuccin-cursors.mochaMauve;
+    name = "Catppuccin-Mocha-Mauve-Cursors";
+    size = 24;
   };
 
   xdg.configFile = {
